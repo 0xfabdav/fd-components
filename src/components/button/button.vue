54174c53f4d2afd5ecf2e00eab802fae26d1b90e
David Hass <david@hasselboeck.de>
2024-08-10 22:52:59 +0200

    Initial commit

---
+++ src/components/button/button.vue
@@ -0,0 +1,27 @@
+<template>
+    <button type="button" :class="{
+        'rounded-md py-1 px-3 font-semibold': true,
+        'default-button': variant === 'normal',
+        'danger-button': variant === 'danger',
+        'disabled-button cursor-not-allowed': disabled,
+        'unfilled-button': !filled,
+    }" @click="emit('click', $event)" :disabled="disabled">
+        {{ label }}
+    </button>
+</template>
+
+<script lang="ts" setup>
+import "./button.css";
+import { PropType } from "vue";
+
+const props = defineProps({
+    label: { type: String, required: true },
+    filled: { type: Boolean, default: true },
+    disabled: { type: Boolean, default: false },
+    variant: { type: String as PropType<"normal" | "danger">, default: "normal" },
+});
+
+const emit = defineEmits<{
+    (e: "click", clickData: any): void;
+}>();
+</script>
